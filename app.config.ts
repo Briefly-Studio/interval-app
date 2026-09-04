aa898d15da6b597b4a0a4f90890d11009127be7b
joseangulo17 <jose3bs@icloud.com>
2026-09-04 16:00:49 -0400

    build: link Interval to Briefly Studios EAS project

--- app.config.ts
+++ app.config.ts
@@ -10,6 +10,7 @@ export default ({ config }: any) => {
 
   return {
     ...config,
+    owner: "briefly-studios",
     extra: {
       ...(config.extra ?? {}),
       intervalEnv,
@@ -17,6 +18,10 @@ export default ({ config }: any) => {
       cognitoRegion,
       cognitoUserPoolId,
       cognitoAppClientId,
+      eas: {
+        ...((config.extra ?? {}).eas ?? {}),
+        projectId: "59dc3a90-e1e5-4ed4-adb1-a7871a8113d9",
+      },
     },
   };
 };
